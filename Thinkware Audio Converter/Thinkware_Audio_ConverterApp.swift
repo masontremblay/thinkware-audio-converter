fab30171bfb44606b914ecbd827a90d610c7d9cf
Mason Tremblay <masontremblay@icloud.com>
2026-04-06 17:41:19 -0600

    Initial Commit

---
+++ Thinkware Audio Converter/Thinkware_Audio_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thinkware_Audio_ConverterApp.swift
+//  Thinkware Audio Converter
+//
+//  Created by Mason Tremblay on 2026-04-06.
+//
+
+import SwiftUI
+
+@main
+struct Thinkware_Audio_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
